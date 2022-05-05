8c3c33acd39445c62a8d257b838f5b46ff12be6a
Yusuf Numan YILMAZ <yusuf.yilmaz@tazebt.com>
2022-05-05 18:20:43 +0300

    fix

--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,34 @@ import react, { useState, useEffect } from "react";
 import Question from "src/components/Question";
 import QuestionInfo from "src/components/QuestionInfo";
 import { questionList } from "src/utils/questions";
+import Keyboard from "react-simple-keyboard";
+import "react-simple-keyboard/build/css/index.css";
 
 export default function Home() {
   const [word, setWord] = useState("");
   const [buy, setBuy] = useState([]);
+  const [buyTwo, setBuyTwo] = useState([]);
   const [click, setClick] = useState(0);
   const [point, setPoint] = useState(0);
   const [totalPoint, setTotalPoint] = useState(0);
   const [questionNumber, setQuestionNumber] = useState(1);
   const [prediction, setPrediction] = useState(false);
   const [isFinish, setFinish] = useState(false);
+  const layout = {
+    default: ["e r t y u ı o p ğ ü {bksp}", "a s d f g h j k l ş i", "z c v b n m ö ç"],
+    shift: [
+      "~ ! @ # $ % ^ &amp; * ( ) _ + {bksp}",
+      "{tab} Q W E R T Y U I O P { } |",
+      '{lock} A S D F G H J K L : " {enter}',
+      "{shift} Z X C V B N M &lt; &gt; ? {shift}",
+      ".com @ {space}",
+    ],
+  };
   const buyLetter = () => {
     const full = buy.every((item) => item !== "");
     let selectedIndex = Math.floor(Math.random() * word.length);
     let lastWord = buy;
+    let lastBuy = buyTwo;
     while (buy[selectedIndex] !== "") {
       if (buy.every((letter) => letter !== "")) break;
       selectedIndex = Math.floor(Math.random() * word.length);
@@ -24,26 +38,28 @@ export default function Home() {
     setPoint((prevState) => prevState - 100);
     if (!full) {
       lastWord[selectedIndex] = word.split("")[selectedIndex];
+      lastBuy[selectedIndex] = "buyed";
     }
     setBuy(lastWord);
+    setBuyTwo(lastBuy);
   };
   const changeLetter = (e, key) => {
     let lastWord = buy;
     lastWord[key] = e.target.value;
     setClick((prevState) => prevState + 1);
     setBuy(lastWord);
-    let inputList = [];
-    const inputs = document.querySelectorAll("input");
-    Object.keys(inputs).map((key) => {
-      if (inputs[key].value === "") {
-        inputList.push(inputs[key]);
-      }
-    });
-    if (key <= word.length - 2) {
-      setTimeout(() => {
-        inputList.find((item) => item.value === "").focus();
-      }, 1);
-    }
+    // let inputList = [];
+    // const inputs = document.querySelectorAll("input");
+    // Object.keys(inputs).map((key) => {
+    //   if (inputs[key].value === "") {
+    //     inputList.push(inputs[key]);
+    //   }
+    // });
+    // if (key <= word.length - 2) {
+    //   setTimeout(() => {
+    //     inputList.find((item) => item.value === "").focus();
+    //   }, 1);
+    // }
   };
   const submit = () => {
     setPrediction(false);
@@ -64,19 +80,37 @@ export default function Home() {
   };
   const predictionHandle = () => {
     setPrediction(true);
-    let inputList = [];
-    const inputs = document.querySelectorAll("input");
-    Object.keys(inputs).map((key) => {
-      if (inputs[key].value === "") {
-        inputList.push(inputs[key]);
-      }
-    });
-    setTimeout(() => {
-      inputList.find((item) => item.value === "").focus();
-    }, 1);
+    // let inputList = [];
+    // const inputs = document.querySelectorAll("input");
+    // Object.keys(inputs).map((key) => {
+    //   if (inputs[key].value === "") {
+    //     inputList.push(inputs[key]);
+    //   }
+    // });
+    // setTimeout(() => {
+    //   inputList.find((item) => item.value === "").focus();
+    // }, 1);
+  };
+  const onChange = (input) => {
+    // console.log("Input changed", input);
+  };
+
+  const onKeyPress = (button) => {
+    const index = buy.findIndex((b) => b === "");
+    if (button === "{bksp}") {
+      let notBuyedIndexes = [];
+      buyTwo.map((bt, i) => {
+        if (bt !== "buyed" && buy[i] !== "") notBuyedIndexes.push(i);
+      });
+      let newIndex = index === -1 ? buy.length : index;
+      changeLetter({ target: { value: "" } }, notBuyedIndexes[notBuyedIndexes.length - 1]);
+    } else {
+      changeLetter({ target: { value: button } }, index);
+    }
   };
   useEffect(() => {
     setBuy(Array(word.length).fill(""));
+    setBuyTwo(Array(word.length).fill(""));
   }, [word]);
   useEffect(() => {
     setWord(questionList.find((q) => q.id === questionNumber).answer);
@@ -102,6 +136,10 @@ export default function Home() {
         </div>
       )}
       {isFinish && <div>Tebrikler! Puanınız : {totalPoint}</div>}
+      <div className="keyboard">
+        {!prediction && <div className="popper"></div>}
+        <Keyboard layout={layout} onChange={onChange} onKeyPress={onKeyPress} />
+      </div>
     </div>
   );
 }
